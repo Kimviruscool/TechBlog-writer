680606691c5c100960d70aa52995de18f32f3452
Kimviruscool <sinsa9122@naver.com>
2026-04-06 21:13:14 +0900

    api py

--- backend/api/gemini.py
+++ backend/api/gemini.py
@@ -1,1 +1,20 @@
 # gemini api
+
+import os
+from google import genai
+from dotenv import load_dotenv
+
+def get_client():
+    # api 클라이언트 초기화 및 유효성 검사
+    load_dotenv()
+    api_key = os.getenv('GEMINI_API_KEY')
+    if not api_key:
+        print("[ERROR] .env 파일에서 GEMINI_API_KEY를 찾을 수 없습니다.")
+        return None
+
+    try :
+        client = genai.GenaiClient(api_key=api_key)
+        return client
+    except Exception as e:
+        print(f"[Error] 클라이언트 생성 실패 {e}")
+        return None
